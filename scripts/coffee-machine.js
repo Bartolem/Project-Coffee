a0b0a72eaaf543133d8e8dba3ae4769a99053484
Bartolem <97914472+Bartolem@users.noreply.github.com>
2022-06-30 19:38:55 +0200

    initial commit

---
+++ scripts/coffee-machine.js
@@ -0,0 +1,35 @@
+let milk = document.querySelector('.milk');
+let coffee = document.querySelector('.coffee');
+let button = document.querySelector('button');
+
+// const chooseType = function() {
+
+// }
+
+button.addEventListener('click', function() {
+    milk.classList.add('pour');
+
+    setTimeout(function() {
+        milk.classList.remove('pour');
+    }, 8000);
+});
+
+
+
+// function pureMilk() {
+
+// }
+
+// function pureCoffee() {
+//     function buttonClick() {
+//         button.addEventListener('click', function() {
+//             coffee.classList.add('pour');
+        
+//             setTimeout(function() {
+//                 coffee.classList.remove('pour');
+//             }, 8000);
+//         });
+//     }
+// }
+
+
