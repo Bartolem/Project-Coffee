85e24d61f64c8b874907eea5f4865bf9c2f4f459
Bartolem <97914472+Bartolem@users.noreply.github.com>
2022-07-02 16:56:05 +0200

    add side buttons for check coffee type and add output for checked type

--- scripts/coffee-machine.js
+++ scripts/coffee-machine.js
@@ -1,12 +1,40 @@
 let milk = document.querySelector('.milk');
 let coffee = document.querySelector('.coffee');
-let button = document.querySelector('.start');
+let startButton = document.querySelector('.start');
+let powerButton = document.querySelector('.power');
+let smallPowerBtn = document.querySelector('.small-btn-power');
+let espressoButton = document.querySelector('.espresso-btn');
+let latteButton = document.querySelector('.latte-btn');
+let americanoButton = document.querySelector('.americano-btn');
+let cappuccinoButton = document.querySelector('.cappuccino-btn');
+let text = document.querySelector('#output-text');
+
+powerButton.addEventListener('click', function() {
+    powerButton.style.color = "green";
+    smallPowerBtn.style.backgroundColor = "green";
+});
+
+espressoButton.addEventListener('click', function() {
+    text.value = "espresso";
+});
+
+latteButton.addEventListener('click', function() {
+    text.value = "latte";
+});
+
+americanoButton.addEventListener('click', function() {
+    text.value = "americano";
+});
+
+cappuccinoButton.addEventListener('click', function() {
+    text.value = "cappuccino";
+});
 
 // const chooseType = function() {
 
 // }
 
-button.addEventListener('click', function() {
+startButton.addEventListener('click', function() {
     milk.classList.add('pour');
 
     setTimeout(function() {
